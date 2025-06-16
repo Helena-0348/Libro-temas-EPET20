fa90787f76b2886eaafd7a161a01edcf7a0da3f2
kiaz-zp <kiaantozap55@gmail.com>
2025-06-15 23:21:24 -0300

    primer componente del inicio

---
+++ src/inicio_components/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//que necesito para el inicio?
+const Header = () => {
+    return {
+        <header style={}>
+        <div style={}>EPET N20</div>
+        
+    }
+}
