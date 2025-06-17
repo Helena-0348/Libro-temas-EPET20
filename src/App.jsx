e12ce7eeaebeefe684f51545f1ed23f8c8a5177b
Helena-0348 <juliahgualda@gmail.com>
2025-06-18 00:45:11 +0200

    eliminando codigo redundante del app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,32 +9,7 @@ import './css/App.css'
 const App = () => {
   return (
     <div>
-      <Encabezado />
-      <div className='cajaElementos' >
-        <div className='cajaOpcion'>
 
-          <Semana />
-          <div className='cajaMaterias'>
-
-            <Materias curso="3ro 3ra" materia="Matematica" />
-            <Materias curso="3ro 3ra" materia="Fisica" />
-            <Materias curso="3ro 3ra" materia="Lengua y lit." />
-            <Materias curso="3ro 3ra" materia="Dibujo tec." />
-            <Materias curso="3ro 3ra" materia="Historia" />
-            <Materias curso="3ro 2da" materia="Ingles" />
-            <Materias curso="3ro 2da" materia="Quimica" />
-            <Materias curso="3ro 2da" materia="Geografia" />
-            <Materias curso="3ro 2ra" materia="Historia" />
-
-          </div>
-
-          <Boton props="Poec" />
-          <Boton props="Editar" />
-        </div>
-
-        <Tabla className='Tabla' />
-
-      </div>
     </div>
   );
 };
