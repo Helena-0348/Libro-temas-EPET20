67e6ae89b3f3d888dd8957ff01a54bcfbc64c60f
Leon Haal <leontomashaal@gmail.com>
2025-06-17 22:57:14 -0300

    Registro

---
+++ src/components/Registro.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/Registro.css';
+
+const Registro = () => {
+  return (
+
+    <div className="registro-container">
+      <h1 className="registro-title">Registro</h1>
+      <div className="registro-form">
+        
+        <input type="text" placeholder="Gmail" className="registro-input" />
+        <input type="text" placeholder="Curso" className="registro-input" />
+        <input type="text" placeholder="División" className="registro-input" />
+        <input type="text" placeholder="Código de Clase" className="registro-input" />
+        <button className="registro-button">Iniciar sesión</button>
+      </div>
+      <div className="registro-logo">
+        <img src="/a.png" alt="E.P.E.T. N° 20 NEUQUÉN" />
+      </div>
+      <Link to="/otra-pagina"  className="registro-link">Ir a otra página</Link>
+    </div>
+
+  );
+};
+
+export default Registro;
