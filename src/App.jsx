08a11d28b574312ce7063a4be1e1dd12ca3ab54f
chocorocoPrograma <rociodlfv@gmail.com>
2025-06-17 21:38:27 -0300

    codigo para app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,59 @@
 import React from 'react'
 
+
+import React from "react";
+import Formulario from "./componentes/ingresoEmail";
+import logo_epet20 from "./recursos/img/logo_epet20.jpg";
+/*import { BrowserRouter as Router } from 'react-router-dom';*/
+
+function App() {
+    return ( 
+        /*
+        <Router>
+           <Routes>
+                 <Route path="/" element={<Formulario titulo="Formulario Email" />} />
+                 <Route path="/resultado" element={<PantallaDestino />} />
+           </Routes>
+        </Router>
+        */
+        <div>
+            <img src={logo_epet20} alt="logo epet20"/>
+            <h1>Iniciar sesión</h1>
+            <Formulario></Formulario>
+        </div>
+
+
+
+    );
+}
+
+export default App;
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
 function App() {
   return (
     <div className="App">
