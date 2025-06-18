3c0a13d24795421b245baaf7f3aa0a64fa15ed2e
chocorocoPrograma <rociodlfv@gmail.com>
2025-06-17 22:05:11 -0300

    enlazando css con componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
-
 import React from "react";
-import Formulario from "./componentes/ingresoEmail";
-import logo_epet20 from "./recursos/img/logo_epet20.jpg";
+import Formulario from "./components/Formulario.jsx";
+import logo_epet20 from "./css/logo_epet20.jpg";
+// import Formulario from './css/Formulario';
+
 /*import { BrowserRouter as Router } from 'react-router-dom';*/
 
 function App() {
@@ -28,51 +27,50 @@ function App() {
 }
 
 export default App;
-import React from 'react'
+// import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
         
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
         
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+
+const Formulario = () =>{
+    //el primer no existe, esta en blanco
+    const[email, setEmail] = useState('');
+
+
+const handleSubmit = (e) =>{
+    //revisa que no se cargue de vuelta
+    e.preventDefault();
+    alert(`${email} inexistente`);
+};
+function Formulario(){
+
+    return(
+        //creando formulario
+       <form onSubmit={handleSubmit}>
+              <div>
+                <label>Correo electrónico</label>
+                <input type="text"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}/>
+              </div>
+       </form>
+       
+    );
+    };
+    
+}
+export default Formulario;
