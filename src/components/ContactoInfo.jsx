404fd534ea85073e26c5ffdac00f3a29cd2f8161
kiaz-zp <kiaantozap55@gmail.com>
2025-09-02 20:28:23 -0300

    Agregando código a el componente ContactoInfo.jsx

---
+++ src/components/ContactoInfo.jsx
@@ -0,0 +1,11 @@
+import '../ContactoInfo.css';
+
+const ContactoInfo = () => {
+  return (
+    <div className="contacto">
+      <p>Lanin 2020 - Tel. (299)4478052</p>
+    </div>
+  );
+};
+
+export default ContactoInfo;
