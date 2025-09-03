464394f822f5e01d5df725976d28b603173b8180
kiaz-zp <kiaantozap55@gmail.com>
2025-09-02 22:13:42 -0300

    Corigiendo rutas relativas de Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
-//import Header from '../components/Encabezado';
-import Info from '../components/Info';
+//import Encabezado from '../components/Encabezado';
+import Info from './components/Info';
 import ContactoInfo from './ContactoInfo';
 import "../css/Home.css";
 
 const Home = () => {
     return (
         <div classname= "Home">
-            <Header />
+            <Encabezado />
             <Info/>
             <ContactoInfo/>
 
