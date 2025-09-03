91308c6af2e590538c4d7f9842e08b8789fb6710
kiaz-zp <kiaantozap55@gmail.com>
2025-09-03 00:51:08 -0300

    Creando y codificando el componente Titulo.jsx

---
+++ src/components/Titulo.jsx
@@ -0,0 +1,13 @@
+import '../css/Titulo.css';
+
+const Titulo = () => {
+  return (
+    <div className="Titulo">
+      <header>
+        Libro de Temas - EPET N°20
+      </header>
+    </div>
+  );
+};
+
+export default Titulo;
